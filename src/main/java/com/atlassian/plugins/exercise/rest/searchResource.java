59ac96fb0de7a1cff625d5f20e751fad98289601
Linas Jurgelėnas <linas.jurgelenas@gmail.com>
2021-01-07 23:58:06 +0200

    working correct response from main function. TODO change Json custom serializer.  problems with POM dependencies.

--- src/main/java/com/atlassian/plugins/exercise/rest/searchResource.java
+++ src/main/java/com/atlassian/plugins/exercise/rest/searchResource.java
@@ -2,9 +2,7 @@ package com.atlassian.plugins.exercise.rest;
 
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
 import com.google.gson.Gson;
-import kong.unirest.GenericType;
 import kong.unirest.Unirest;
-import kong.unirest.UnirestParsingException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,21 +10,29 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static kong.unirest.Cache.builder;
 
 /**
  * A resource of message.
  */
 @Path("/message")
 public class searchResource {
+    private Long CACHE_TIME = 3L;
+    private TimeUnit CACHE_TIME_UNIT = TimeUnit.MINUTES;
+    private String BASE_URL = "https://itunes.apple.com/search";
+    private String USERNAME = "ADMIN";//kol kas hardcode'inta
+    private String PASSWORD = "ADMIN";
+
 
     public Response getMessage(@QueryParam("key") String key)
     {
-        if (key!=null) {
+        if (key != null) {
             return Response.ok(new searchResourceModel(key, getMessageFromKey(key))).build();
         }
         else {
-            return Response.ok(new searchResourceModel("default","Hello World")).build();
+            return Response.ok(new searchResourceModel("default",null)).build();
         }
     }
 
@@ -40,35 +46,33 @@ public class searchResource {
     }
 
     public String getMessageFromKey(String key) {//todo i private
+        if (key == null) {
+            return "No Key value passed. Please input value.";
+        }
 
-        Artists artists = Unirest.get("https://itunes.apple.com/search")
+        Unirest.config().cacheResponses(builder()
+                .maxAge(CACHE_TIME, CACHE_TIME_UNIT)); // Max age is how long the entry will be kept.
+
+        //todo if no artists fetched
+        Artists artists = Unirest.get(BASE_URL)
+            //    .basicAuth(USERNAME, PASSWORD) //todo check authentification
                 .queryString("entity", "allArtist")
                 .queryString("term", key)
                 .asObject(Artists.class)
                 .getBody();
 
-        return new Gson().toJson(artists);
-		/*HttpClient httpclient = HttpClients.createDefault();
-		HttpPost httpPost = new HttpPost("https://itunes.apple.com/search?");
+        /*Unirest.get("http://somewhere")
+                .asJson()
+                .ifSuccess(response -> someSuccessMethod(response))
+                .ifFailure(response -> {
+                    log.error("Oh No! Status" + response.getStatus());
+                    response.getParsingError().ifPresent(e -> {
+                        log.error("Parsing Exception: ", e);
+                        log.error("Original body: " + e.getOriginalBody());
+                    });
+                });*/
 
-		// Request parameters and other properties.
-		List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-		params.add(new BasicNameValuePair("entity", "allArtist"));
-		params.add(new BasicNameValuePair("term", key));
-		httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
 
-		//Execute and get the response.
-		HttpResponse response = httpclient.execute(httpPost);
-		HttpEntity entity = response.getEntity();
-
-		if (entity != null) {
-			try (InputStream instream = entity.getContent()) {
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				response.getEntity().writeTo(out);
-				String responseString = out.toString();
-				out.close();
-			}
-		}
-        return key;*/
+        return new Gson().toJson(artists);//todo use custom deserializer
     }
 }
